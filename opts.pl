f489c596616c252506885903c67e6e1d1cbd78e6
Jesse Allen <the3dfxdude@gmail.com>
2009-11-06 18:26:57 -0700

    build: Explain the build opts more.

--- opts.pl
+++ opts.pl
@@ -1,12 +1,18 @@
-$wine_include_path = "/usr/local/include/wine";
 
+# Set $wine_prefix to the prefix where wine is installed.
+# This is typically "/usr".
+$wine_prefix = "/usr";
+
+# This sets up the include paths.
 our @includes = (
 "include",
-"$wine_include_path/windows",
-"$wine_include_path/msvcrt"
+"$wine_prefix/include/wine/windows",
+"$wine_prefix/include/wine/msvcrt"
 );
 @includes = map { "-I$_" } @includes;
 
+# The following sets flags used during compiling.
+# The flags that are known to work:
 # AMPLUS
 # DISABLE_MULTI_PLAYER
 # FRENCH
