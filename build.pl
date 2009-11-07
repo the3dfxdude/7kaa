2acbe320b3c9d6107a3b1261ad88386db41f284e
Jesse Allen <the3dfxdude@gmail.com>
2009-11-06 18:21:22 -0700

    Build: move asm building into the top level script

--- build.pl
+++ build.pl
@@ -2,8 +2,26 @@
 
 use File::stat;
 require "files.pl";
-require "asm/files.pl";
 require "opts.pl";
+
+chdir "asm" or die "Missing asm dir";
+foreach (@asm_files) {
+  my $flag = 1;
+
+  # check modification time
+  if (-e "$_.o") {
+    my $objstat = stat("$_.o");
+    my $cppstat = stat("$_.asm");
+    $cppstat->mtime >= $objstat->mtime or $flag = 0;
+  }
+
+  if ($flag) {
+    my $cmd = "jwasm -q -elf -zt1 $_.asm";
+    print "$cmd\n";
+    system $cmd and die "jwasm failed";
+  }
+}
+chdir "..";
 
 foreach (@c_files) {
   my $flag = 1;
