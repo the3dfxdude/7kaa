20ab2cd06fa639c096be4a327c3a96ced71833e5
Jesse Allen <the3dfxdude@gmail.com>
2010-02-28 14:59:46 -0700

    I_CTRL is needed for the asm build still.

--- src/client/targets.pl
+++ src/client/targets.pl
@@ -101,7 +101,7 @@ ico
 @obj_files = map { "$_.o" } @c_files;
 unless ($no_asm) {
 my @asm_obj_files = qw(
-I_BAR   IB_TRD IB_A    IC       IC_R     CRC     IJ_T
+I_BAR   I_CTRL IB_TRD  IB_A     IC       IC_R    CRC      IJ_T
 IB_T    I_READ I_EMASK IB_TD    I_EREMAP IB_ATRD IB_AT    IB_32
 IB_ATD  IB_R   IB      IR_BAR   IB2      I_LINE  IB_ATR   IB_AR
 IB_TR   IB_DW  I_PIXEL I_FREMAP I_BLACK  I_SNOW  IB_ATRDM IB_TRDM
--- src/server/targets.pl
+++ src/server/targets.pl
@@ -85,7 +85,7 @@ ico
 ### Linking targets ###
 @obj_files = map { "$_.o" } @c_files;
 my @asm_obj_files = qw(
-I_BAR   IB_TRD IB_A    IC       IC_R     CRC     IJ_T
+I_BAR   I_CTRL IB_TRD  IB_A     IC       IC_R    CRC      IJ_T
 IB_T    I_READ I_EMASK IB_TD    I_EREMAP IB_ATRD IB_AT    IB_32
 IB_ATD  IB_R   IB      IR_BAR   IB2      I_LINE  IB_ATR   IB_AR
 IB_TR   IB_DW  I_PIXEL I_FREMAP I_BLACK  I_SNOW  IB_ATRDM IB_TRDM
