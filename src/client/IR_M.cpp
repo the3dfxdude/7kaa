204d1612187619869d55e5dd18602680161473c0
Alex Streit <alex.streit@gmail.com>
2010-02-28 10:27:27 +1000

    IR_M.cpp (IMGremapHMirror) converted to C++

---
+++ src/client/IR_M.cpp
@@ -0,0 +1,64 @@
+/*
+ * Seven Kingdoms: Ancient Adversaries
+ *
+ * Copyright 1997,1998 Enlight Software Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ *Filename    : IR_M.ASM
+ *Description : Remap a bitmap on vga image buffer, with mirroring
+ *
+ * converted to C++
+ */
+
+
+#include <IMGFUN.h>
+#include <COLCODE.h>
+
+
+//--------- BEGIN OF FUNCTION IMGremapHMirror -----------
+//
+// Remap on the VGA screen
+//
+// Note : No border checking is made in this function.
+//	 Placing an icon outside image buffer will cause serious BUG.
+//
+// char *imageBuf   - the pointer to the display surface buffer
+// int  pitch       - the pitch of the display surface buffer
+// int  x1,y1         - the top left vertex of the bar
+// char *bitmapPtr  - the pointer to the bitmap array
+// char **colorTableArray - the pointer to the scale 0 of remap table array
+//
+
+void _stdcall IMGremapHMirror(char* imageBuf, int pitch, int x, int y, char* bitmapPtr, unsigned char** colorTableArray)
+{
+	int destline = y * pitch + x;
+	int width = ((unsigned char*)bitmapPtr)[0] + (((unsigned char*)bitmapPtr)[1]<<8);
+	int height = ((unsigned char*)bitmapPtr)[2] + (((unsigned char*)bitmapPtr)[3]<<8);
+	int srcline_end = 4 + width - 1;		// skip 4 byte header, points to last pixel on a line
+	int al;
+
+	for ( int j=0; j<height; ++j, destline+=pitch, srcline_end+=width )
+	{
+		for ( int i=0; i<width; ++i )
+		{
+			al = bitmapPtr[ srcline_end - i ];		// NOTE: must be signed
+			((unsigned char*)imageBuf)[ destline + i ] =
+				colorTableArray[ al ][ imageBuf[destline+i] ];
+		}
+	}
+}
+
+//----------- END OF FUNCTION IMGremapHMirror ----------
