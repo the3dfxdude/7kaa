ccc472c5e0cafe439fa67ae37e688fbf98ada8f6
Unavowed <unavowed@vexillium.org>
2010-04-20 20:48:28 +0200

    Implement FileInputStream

---
+++ include/file_input_stream.h
@@ -0,0 +1,42 @@
+/*
+ * Seven Kingdoms: Ancient Adversaries
+ * FileInputStream 
+ *
+ * Copyright 2010 Unavowed <unavowed@vexillium.org>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+#ifndef MEM_INPUT_STREAM_H
+#define MEM_INPUT_STREAM_H
+
+#include <OFILE.h>
+#include <input_stream.h>
+
+class FileInputStream: public InputStream
+{
+private:
+	File *file;
+
+public:
+	FileInputStream();
+	~FileInputStream();
+	bool open(File *file);
+	bool open(const char *file_name);
+	long read(void *buffer, long length);
+	bool seek(long offset, int whence);
+	void close();
+};
+
+#endif
---
+++ src/common/file_input_stream.cpp
@@ -0,0 +1,74 @@
+/*
+ * Seven Kingdoms: Ancient Adversaries
+ * FileInputStream 
+ *
+ * Copyright 2010 Unavowed <unavowed@vexillium.org>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include <file_input_stream.h>
+#include <file_util.h>
+
+FileInputStream::FileInputStream()
+{
+	this->file = NULL;
+}
+
+FileInputStream::~FileInputStream()
+{
+	this->close();
+}
+
+long FileInputStream::read(void *buffer, long length)
+{
+	if (this->file == NULL)
+		return 0;
+}
+
+bool FileInputStream::seek(long offset, int whence)
+{
+	if (this->file == NULL)
+		return false;
+
+	return ::seek(this->file, offset, whence);
+}
+
+bool FileInputStream::open(File *file)
+{
+	this->close();
+	this->file = file;
+	return (file == NULL);
+}
+
+bool FileInputStream::open(const char *file_name)
+{
+	this->close();
+	this->file = new File;
+
+	if (!this->file->file_open(file_name))
+	{
+		this->close();
+		return false;
+	}
+
+	return true;
+}
+
+void FileInputStream::close()
+{
+	delete this->file;
+	this->file = NULL;
+}
