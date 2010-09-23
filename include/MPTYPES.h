9c197385c9dda531131941c5cf220c0be5093e76
Aleksey V <runtime00@gmail.com>
2010-09-23 16:38:41 +0300

    MPTypes: remove remaining IMMPlay and DPlay defines

--- include/MPTYPES.h
+++ include/MPTYPES.h
@@ -34,29 +34,5 @@
 #define SessionIdType uint32_t
 #define PlayerDesc DPPlayer
 
-/*
-#ifdef IMAGICMP
-	#define MultiPlayerType MultiPlayerIM
-	#define mp_obj mp_im
-	#define BROADCAST_PID IMMBROADCAST
-	#define PID_TYPE IMMPID
-	#define ServiceProviderDesc IMServiceProvider
-	#define ServiceIdType DWORD
-	#define SessionDesc IMSessionDesc
-	#define SessionIdType DWORD
-	#define PlayerDesc IMPlayer
-#else
-	#define MultiPlayerType MultiPlayerDP
-	#define mp_obj mp_dp
-	#define BROADCAST_PID DPID_ALLPLAYERS
-	#define PID_TYPE DPID
-	#define ServiceProviderDesc DPServiceProvider
-	#define ServiceIdType GUID
-	#define SessionDesc DPSessionDesc
-	#define SessionIdType GUID
-	#define PlayerDesc DPPlayer
-#endif
-*/
-
 #endif
 
