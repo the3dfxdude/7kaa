the3dfxdude/7kaa

commit 44cc7c71041c93f8b8e604fab0b0a2fb0457f0b2

Main menu's: The games main menu's now correctly respond to exit requests, such as closing the window

src/client/OGFILEA.cpp
@@ -313,6 +313,12 @@ int GameFileArray::menu(int actionMode, int *recno)
 
 		mouse.get_event();
 
+		if( sys.signal_exit_flag == 1 )
+		{
+			retFlag = 0;
+			break;
+		}
+
 		// --------- display ----------//
 
 		if( refreshFlag )

include/OSYS.h
@@ -83,7 +83,7 @@ public:
 
 	char		sys_flag;
 	char		init_flag;
-	char		signal_exit_flag;
+	char		signal_exit_flag; // 0 - not exiting; 1 - exit to OS; 2 - exit to main menu
 	char		need_redraw_flag;    // set to 1 if task switched back. After redraw, clear it
 	char		toggle_full_screen_flag;
 	char		cheat_enabled_flag;

src/client/OBUTTON.cpp
@@ -465,6 +465,11 @@ void Button::wait_press(int timeOut)
 		sys.yield();
 		mouse.get_event();
 
+		if( sys.signal_exit_flag == 1 )
+		{
+			break;
+		}
+
 		//--- when the user is inactive for a certain time, ----//
 		//--------- close the report automatically -------------//
 

src/client/OGAMEMP.cpp
@@ -722,6 +722,8 @@ void Game::multi_player_game(int lobbied, char *game_host)
 	init();
 	remote.handle_vga_lock = 0;	// disable lock handling
 
+	sys.signal_exit_flag = 0; // Richard 24-12-2013: If player tried to exit just as the game loaded, cancel the exit request
+
 	battle.run(nationPara, mpPlayerCount);
 
 	mem_del(nationPara);
@@ -932,6 +934,8 @@ void Game::load_mp_game(char *fileName, int lobbied, char *game_host)
 //	init();
 	remote.handle_vga_lock = 0;	// disable lock handling
 
+	sys.signal_exit_flag = 0; // Richard 24-12-2013: If player tried to exit just as the game loaded, cancel the exit request
+
 	battle.run_loaded();		// 1-multiplayer game
 
 	remote.deinit();
@@ -994,6 +998,12 @@ int Game::mp_select_service()
 
 		sys.yield();
 		mouse.get_event();
+
+		if (sys.signal_exit_flag == 1)
+		{
+			choice = 0;
+			break;
+		}
 
 		if( refreshFlag )
 		{
@@ -1157,6 +1167,12 @@ int Game::mp_select_mode(char *defSaveFileName)
 		sys.yield();
 		mouse.get_event();
 
+		if( sys.signal_exit_flag == 1 )
+		{
+			rc = 0;
+			break;
+		}
+
 		if( refreshFlag )
 		{
 			if( refreshFlag & SMOPTION_PAGE )
@@ -1315,6 +1331,11 @@ int Game::input_box(const char *tell_string, char *buf, int len)
 		sys.yield();
 		mouse.get_event();
 
+		if( sys.signal_exit_flag == 1 )
+		{
+			break;
+		}
+
 		input_box.detect();
 
 		if (buttonOk.detect(KEY_RETURN)) {
@@ -1422,6 +1443,12 @@ int Game::mp_select_session()
 
 		sys.yield();
 		mouse.get_event();
+
+		if( sys.signal_exit_flag == 1 )
+		{
+			choice = 0;
+			break;
+		}
 
 		if( refreshFlag )
 		{
@@ -1719,6 +1746,7 @@ int Game::mp_get_leader_board()
 
 	vga_front.unlock_buf();
 
+	ret = 0;
 	while (1)
 	{
 		vga_front.lock_buf();
@@ -1728,6 +1756,11 @@ int Game::mp_get_leader_board()
 
 		sys.yield();
 		mouse.get_event();
+		
+		if( sys.signal_exit_flag == 1 )
+		{
+			break;
+		}
 
 		if (buttonCancel.detect(buttonCancel.str_buf[0], KEY_ESC) ||
 		    mouse.any_click(1))     // detect right button only when the button is "Cancel"
@@ -2232,6 +2265,8 @@ int Game::mp_select_option(NewNationPara *nationPara, int *mpPlayerCount)
 
 		sys.yield();
 		mouse.get_event();
+
+		// Note: sys.signal_exit_flag is detected at the same point as the cancel/abort button
 
 		// -------- display ----------//
 		// ##### begin Gilbert 25/10 #####//
@@ -3353,7 +3388,7 @@ int Game::mp_select_option(NewNationPara *nationPara, int *mpPlayerCount)
 				}
 			}
 		}
-		else if( returnButton.detect() )
+		else if( returnButton.detect() || (sys.signal_exit_flag == 1) ) // Richard 24-12-2013: signal_exit_flag works as cancel at this stage
 		{
 			if( remote.is_host )
 			{
@@ -4096,6 +4131,8 @@ int Game::mp_select_load_option(char *fileName)
 		sys.yield();
 		mouse.get_event();
 
+		// Note: sys.signal_exit_flag is detected at the same point as the cancel/abort button
+
 		// -------- display ----------//
 		if( refreshFlag || mRefreshFlag )
 		{
@@ -4706,7 +4743,7 @@ int Game::mp_select_load_option(char *fileName)
 				}
 			}
 		}
-		else if( returnButton.detect() )
+		else if( returnButton.detect() || (sys.signal_exit_flag == 1) ) // Richard 24-12-2013: signal_exit_flag works as cancel at this stage
 		{
 			if( remote.is_host )
 			{

src/client/OGAMENCY.cpp
@@ -185,6 +185,11 @@ void Game::view_encyclopedia()
 		mouse.get_event();
 		sys.blt_virtual_buf();
 
+		if( sys.signal_exit_flag == 1 )
+		{
+			break;
+		}
+
 		//------ detect main class buttons -------//
 
 		int selClass = detect_main_class_button();

src/client/OGAMMAIN.cpp
@@ -225,7 +225,8 @@ void Game::main_menu()
 
 		optionInfo = main_option_array;
 
-		sys.signal_exit_flag = 0;
+		// Reset exit-to-main-menu flag
+		if (sys.signal_exit_flag == 2) sys.signal_exit_flag = 0;
 
 		for( i=0 ; i<MAIN_OPTION_COUNT ; i++, optionInfo++ )
 		{
@@ -432,6 +433,11 @@ void Game::single_player_menu()
 	{
 		sys.yield();
 		mouse.get_event();
+
+		if( sys.signal_exit_flag == 1 )
+		{
+			break;
+		}
 
 		if( refreshFlag )
 		{
@@ -682,6 +688,11 @@ void Game::multi_player_menu(int lobbied, char *game_host)
 	{
 		sys.yield();
 		mouse.get_event();
+
+		if( sys.signal_exit_flag == 1 )
+		{
+			break;
+		}
 
 		if( refreshFlag )
 		{

src/client/OGAMSCE2.cpp
@@ -215,6 +215,12 @@ int Game::select_scenario(int scenCount, ScenInfo* scenInfoArray)
 
 		mouse.get_event();
 
+		if( sys.signal_exit_flag == 1 )
+		{
+			retFlag = 0;
+			break;
+		}
+
 		// --------- display ----------//
 
 		if( refreshFlag )

src/client/OGAMSING.cpp
@@ -496,21 +496,6 @@ static int select_option()
 
 	while(1)
 	{
-#ifndef NO_WINDOWS  // FIXME
-		MSG msg;
-		if (PeekMessage( &msg, NULL, 0, 0, PM_NOREMOVE))
-		{
-			if (!GetMessage( &msg, NULL, 0, 0))
-			{
-				sys.signal_exit_flag = 1;
-				// BUGHERE : vga_front is unlocked
-				return 0;
-			}
-			TranslateMessage(&msg);
-			DispatchMessage(&msg);
-			continue;
-		}
-#endif
 		if( sys.need_redraw_flag )
 		{
 			refreshFlag = SGOPTION_ALL;
@@ -521,6 +506,12 @@ static int select_option()
 
 		sys.yield();
 		mouse.get_event();
+
+		if( sys.signal_exit_flag == 1 )
+		{
+				retFlag = 0;
+				break;
+		}
 
 		// -------- display ----------//
 		if( refreshFlag )

commit 3bc18dd7dd8b5077cbd91f80d27aec1e01de3871

Game menu: The Load menu now works properly again ingame. Fixes a bug introduced in 44cc7c71041.
The Sys::load_game function sets signal_exit_flag, but this caused the load menu to immediately
close again.

src/client/OGFILEA.cpp
@@ -313,6 +313,7 @@ int GameFileArray::menu(int actionMode, int *recno)
 
 		mouse.get_event();
 
+		// When called ingame sys.signal_exit_flag is set to 2 by Sys::load_game
 		if( sys.signal_exit_flag == 1 )
 		{
 			retFlag = 0;

src/client/OSYS.cpp
@@ -2472,7 +2472,7 @@ void Sys::load_game()
    if( remote.is_enable() )
       return;
 
-   signal_exit_flag=1;     // for deinit functions to recognize that this is an end game deinitialization instead of a normal deinitialization
+   signal_exit_flag=2;     // for deinit functions to recognize that this is an end game deinitialization instead of a normal deinitialization
 
    int rc=0;
 
@@ -2486,7 +2486,10 @@ void Sys::load_game()
       case 0:
          signal_exit_flag = 0;
          break;
-         // case -1 and otherwise, left sys.signal_exit_flag 1 to exit the game
+	  
+	  default:
+		 // case -1 and otherwise, set sys.signal_exit_flag to 1 to exit the game
+		 sys.signal_exit_flag = 1;
    }
 
    game_file_array.menu(-1);               // restore screen area from back buffer