the3dfxdude/7kaa

commit ea7e7451230de0471227f6ea5b0e53976d2205c5

Sys: Add detection for a game config directory. Future place for saves, config, and high scores. Tested on linux and wine.

src/client/OSYS.cpp
@@ -82,6 +82,15 @@
 #include <OOPTMENU.h>
 #include <OINGMENU.h>
 // ##### end Gilbert 23/10 ######//
+#ifdef NO_WINDOWS
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <errno.h>
+#endif
+
+#include <dbglog.h>
+
+DBGLOG_DEFAULT_CHANNEL(Sys);
 
 //----------- Declare static functions -----------//
 
@@ -148,6 +157,7 @@ int Sys::init()
 
 //	debug_session       = m.is_file_exist("DEBUG.SYS");
 
+   set_config_dir(); // where saves, config.dat, and hall of fame are kept
 	set_game_dir();      // set game directories names and game version
 
    //------- initialize more stuff ---------//
@@ -424,6 +434,53 @@ void Sys::deinit_objects()
    game_file_array.deinit();
 }
 //------- End of function Sys::deinit_objects -----------//
+
+
+//-------- Begin of function Sys::set_config_dir --------//
+//
+int Sys::set_config_dir()
+{
+#ifdef NO_WINDOWS
+   const char *default_config_dir = "/.7kaa";
+   const char *home_env_var = "HOME";
+#else // WINDOWS
+   const char *default_config_dir = "\\.7kaa";
+   const char *home_env_var = "USERPROFILE";
+#endif 
+   int r;
+
+   // Find the path for the config directory--this is based on the default
+   // location for the logged in user.
+   char *home = getenv(home_env_var);
+
+   if (strlen(home) + strlen(default_config_dir) >= MAX_PATH)
+   {
+      ERR("Game config dir path too long.\n");
+      return 0;
+   }
+
+   strcpy(dir_config, home);
+   strcat(dir_config, default_config_dir);
+
+   MSG("Game config dir path: %s\n", dir_config);
+
+   // create the config directory
+#ifdef NO_WINDOWS
+   r = mkdir(dir_config, 0777) == -1 ? errno == EEXIST : 1;
+#else // WINDOWS
+   r = !CreateDirectory(dir_config, NULL) ?
+       GetLastError() == ERROR_ALREADY_EXISTS : 1;
+#endif
+
+   if (!r)
+   {
+      ERR("Unable to acquire a usable game config dir.\n");
+      return 0;
+   }
+
+   return 1;
+}
+//------- End of function Sys::set_config_dir -----------//
 
 
 //-------- Begin of function Sys::run --------//

src/server/OSYS.cpp
@@ -82,6 +82,15 @@
 #include <OOPTMENU.h>
 #include <OINGMENU.h>
 // ##### end Gilbert 23/10 ######//
+#ifdef NO_WINDOWS
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <errno.h>
+#endif
+
+#include <dbglog.h>
+
+DBGLOG_DEFAULT_CHANNEL(Sys);
 
 //----------- Declare static functions -----------//
 
@@ -148,6 +157,7 @@ int Sys::init()
 
 //	debug_session       = m.is_file_exist("DEBUG.SYS");
 
+   set_config_dir(); // where saves, config.dat, and hall of fame are kept
 	set_game_dir();      // set game directories names and game version
 
    //------- initialize more stuff ---------//
@@ -424,6 +434,53 @@ void Sys::deinit_objects()
    game_file_array.deinit();
 }
 //------- End of function Sys::deinit_objects -----------//
+
+
+//-------- Begin of function Sys::set_config_dir --------//
+//
+int Sys::set_config_dir()
+{
+#ifdef NO_WINDOWS
+   const char *default_config_dir = "/.7kaa";
+   const char *home_env_var = "HOME";
+#else // WINDOWS
+   const char *default_config_dir = "\\.7kaa";
+   const char *home_env_var = "USERPROFILE";
+#endif 
+   int r;
+
+   // Find the path for the config directory--this is based on the default
+   // location for the logged in user.
+   char *home = getenv(home_env_var);
+
+   if (strlen(home) + strlen(default_config_dir) >= MAX_PATH)
+   {
+      ERR("Game config dir path too long.\n");
+      return 0;
+   }
+
+   strcpy(dir_config, home);
+   strcat(dir_config, default_config_dir);
+
+   MSG("Game config dir path: %s\n", dir_config);
+
+   // create the config directory
+#ifdef NO_WINDOWS
+   r = mkdir(dir_config, 0777) == -1 ? errno == EEXIST : 1;
+#else // WINDOWS
+   r = !CreateDirectory(dir_config, NULL) ?
+       GetLastError() == ERROR_ALREADY_EXISTS : 1;
+#endif
+
+   if (!r)
+   {
+      ERR("Unable to acquire a usable game config dir.\n");
+      return 0;
+   }
+
+   return 1;
+}
+//------- End of function Sys::set_config_dir -----------//
 
 
 //-------- Begin of function Sys::run --------//

include/OSYS.h
@@ -124,6 +124,7 @@ public:
 
 	char  	cdrom_drive;
 
+	char    dir_config[MAX_PATH+1];
 	char  	dir_image[MAX_PATH+1];
 	char  	dir_encyc[MAX_PATH+1];
 #ifdef AMPLUS
@@ -187,6 +188,7 @@ public:
 
 	//---- for setting game directories ----//
 
+	int 		set_config_dir();
 	void 		set_game_dir();
 	int 		set_one_dir( const char* checkFileName, const char* defaultDir, char* trueDir );
 	void 		get_cdrom_drive();

commit f91b1732d59117a808bc1548afef6ed67e4850d4

Fixed AUTO2.SAV bug introduced in [ea7e7451230de Sys: Add detection for a game config directory].
This patch fixes AUTO2.SAV never being created/used for autosaves.
The Sys::auto_save function was still using relative paths for save games, rather than the
dir_config directory.

src/client/OSYS.cpp
@@ -464,11 +464,11 @@ void Sys::run(int isLoadedGame)
 static void test_lzw()
 {
    // test lzw compress
-   if( misc.is_file_exist("NORMAL.SAV"))
+   if( misc.is_file_exist("NORMAL.SAV")) // BUGHERE: Should use a full path, using sys.dir_config
    {
       File f,g;
       Lzw lzw_c, lzw_d;    // one for compress, the other for decompress
-      f.file_open("NORMAL.SAV");
+      f.file_open("NORMAL.SAV"); // BUGHERE: <same as above>
 
       // read into buffer
       long fileSize = f.file_size();
@@ -891,14 +891,25 @@ void Sys::auto_save()
       }
       else
       {
+         //---------- get path to savegames ----------//
+
+         char auto1_path[MAX_PATH+1], auto2_path[MAX_PATH+1];
+
+         if (misc.path_cat(auto1_path, dir_config, "AUTO.SAV", MAX_PATH+1) == 0 ||
+             misc.path_cat(auto2_path, dir_config, "AUTO2.SAV", MAX_PATH+1) == 0)
+         {
+	        ERR("Path to the savegames too long.\n");
+            return;
+         }
+
          //--- rename the existing AUTO.SAV to AUTO2.SAV and save a new game ---//
 
-         if( misc.is_file_exist( "AUTO.SAV" ) )
+         if( misc.is_file_exist( auto1_path ) )
          {
-            if( misc.is_file_exist( "AUTO2.SAV" ) )      // if there is already an AUTO2.SAV, delete it
-               remove( "AUTO2.SAV" );
+            if( misc.is_file_exist( auto2_path ) )      // if there is already an AUTO2.SAV, delete it
+               remove( auto2_path );
 
-            rename( "AUTO.SAV", "AUTO2.SAV" );
+            rename( auto1_path, auto2_path );
          }
       }
 
@@ -930,14 +941,25 @@ void Sys::auto_save()
       day_frame_count==0 && info.game_day==1 && info.game_month%2==0 )
 	// ###### patch end Gilbert 23/1 #######//
    {
+	  //---------- get path to savegames ----------//
+
+      char auto1_path[MAX_PATH+1], auto2_path[MAX_PATH+1];
+
+	  if (misc.path_cat(auto1_path, dir_config, "AUTO.SVM", MAX_PATH+1) == 0 ||
+          misc.path_cat(auto2_path, dir_config, "AUTO2.SVM", MAX_PATH+1) == 0)
+      {
+	     ERR("Path to the savegames too long.\n");
+         return;
+      }
+
       //--- rename the existing AUTO.SVM to AUTO2.SVM and save a new game ---//
 
-      if( misc.is_file_exist( "AUTO.SVM" ) )
+      if( misc.is_file_exist( auto1_path ) )
       {
-         if( misc.is_file_exist( "AUTO2.SVM" ) )      // if there is already an AUTO2.SVM, delete it
-            remove( "AUTO2.SVM" );
+         if( misc.is_file_exist( auto2_path ) )      // if there is already an AUTO2.SVM, delete it
+            remove( auto2_path );
 
-         rename( "AUTO.SVM", "AUTO2.SVM" );
+         rename( auto1_path, auto2_path );
       }
 
       game_file.save_game( "AUTO.SVM" );