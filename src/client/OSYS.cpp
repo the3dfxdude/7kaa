the3dfxdude/7kaa

commit 96101eed6ad1c7bc13bf44acf2805bcf413503c4

Implement network support using enet.
Lobby features have been disabled.

SDL_net has been disabled.

src/client/OSYS.cpp
@@ -1245,20 +1245,15 @@ int Sys::is_mp_sync(int *unreadyPlayerFlag)
 
    //------- if some remote machines are not ready yet -------//
 
-   if( nationRecno>0 )
+   if( nationRecno>0 && !ec_remote.is_player_valid(nationRecno) )
    {
-      DEBUG_LOG("a nation not ready");
+      DEBUG_LOG("Connection Lost");
       DEBUG_LOG(nationRecno);
 
-      if (misc.get_time() >= last_frame_time + CONNECTION_LOST_TIME_OUT)
-      {
-         //---- if it has been time out for too long, carry out connection lost handling ---//
-         ec_remote.set_player_lost(nationRecno);
-         DEBUG_LOG("Connection Lost");
-         news_array.multi_connection_lost(nationRecno);
-         nationPtr->nation_type = NATION_AI;    // let computer take over the nation
-         nation_array.ai_nation_count++;
-      }
+      //---- the connection was lost with a remote player, let the ai take over ----//
+      news_array.multi_connection_lost(nationRecno);
+      nationPtr->nation_type = NATION_AI;
+      nation_array.ai_nation_count++;
 
       return 0;
    }

configure.ac
@@ -80,7 +80,7 @@ dnl AC_CHECK_FUNCS([])
 audio_backend=openal # openal dsound
 input_backend=sdl    # sdl dinput none
 video_backend=sdl    # sdl ddraw none
-net_backend=sdlnet   # sdlnet none
+net_backend=enet   # enet none
 use_windows=no
 use_mingw=no
 
@@ -153,7 +153,7 @@ AM_CONDITIONAL([INPUT_DINPUT], [test "$input_backend" = dinput])
 AM_CONDITIONAL([INPUT_NONE],   [test "$input_backend" = none])
 AM_CONDITIONAL([INPUT_SDL],    [test "$input_backend" = sdl])
 AM_CONDITIONAL([NET_NONE],     [test "$net_backend" = none])
-AM_CONDITIONAL([NET_SDLNET],   [test "$net_backend" = sdlnet])
+AM_CONDITIONAL([NET_ENET],     [test "$net_backend" = enet])
 AM_CONDITIONAL([VIDEO_DDRAW],  [test "$video_backend" = ddraw])
 AM_CONDITIONAL([VIDEO_NONE],   [test "$video_backend" = none])
 AM_CONDITIONAL([VIDEO_SDL],    [test "$video_backend" = sdl])
@@ -200,6 +200,12 @@ AS_IF([test "$input_backend" = sdl \
 AS_IF([test "$net_backend" = sdlnet], [
   SEARCH_LIB_FLAGS([SDLNet_Init], ["-lSDL_net -lwsock32" -lSDL_net], [], [
     AC_ERROR([SDL_net not found])
+  ])
+])
+
+AS_IF([test "$net_backend" = enet], [
+  SEARCH_LIB_FLAGS([enet_initialize], ["-lenet -lwsock32" -lenet], [], [
+    AC_ERROR([enet not found])
   ])
 ])
 
@@ -299,9 +305,9 @@ AS_CASE(["$net_backend"],
       Define to 1 to disable multiplayer
     ])
   ],
-  [sdlnet], [
-    AC_DEFINE([USE_SDLNET], [1], [
-      Define to 1 if using SDL_net as the network backend
+  [enet], [
+    AC_DEFINE([USE_ENET], [1], [
+      Define to 1 if using enet as the network backend
     ])
   ], [
     AC_ERROR([Unknown network backend: $net_backend])

include/multiplayer.h
@@ -2,7 +2,7 @@
  * Seven Kingdoms: Ancient Adversaries
  *
  * Copyright 1997,1998 Enlight Software Ltd.
- * Copyright 2010,2011 Jesse Allen
+ * Copyright 2010,2011,2013 Jesse Allen
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -29,8 +29,7 @@
 #include <player_desc.h>
 #include <ODYNARRB.h>
 #include <stdint.h>
-#include <network.h>
-#include <queue>
+#include <enet/enet.h>
 
 #define MP_SERVICE_PROVIDER_NAME_LEN 64
 #define MP_SESSION_NAME_LEN 64
@@ -38,125 +37,6 @@
 #define MP_RECV_BUFFER_SIZE 0x2000
 #define MP_GAME_LIST_SIZE 10
 #define MP_LADDER_LIST_SIZE 6
-
-#define MP_STATUS_IDLE 0
-#define MP_STATUS_CONNECTING 1
-#define MP_STATUS_PREGAME 2
-#define MP_STATUS_INGAME 3
-#define MP_STATUS_REQ_GAME_LIST 4
-#define MP_STATUS_REQ_LADDER 5
-
-enum
-{
-	MPMSG_GAME_BEACON = 0x1f4a0001,
-	MPMSG_REQ_GAME_LIST,
-	MPMSG_GAME_LIST,
-	MPMSG_VERSION_ACK,
-	MPMSG_VERSION_NAK,
-	MPMSG_CONNECT,
-	MPMSG_CONNECT_ACK,
-	MPMSG_REQ_LADDER,
-	MPMSG_LADDER,
-	MPMSG_NEW_PEER_ADDRESS,
-};
-
-struct mp_msg
-{
-	uint32_t player_id;
-	uint16_t size;
-	struct packet_header header;
-	char *data;
-};
-
-#pragma pack(1)
-struct MsgHeader
-{
-	uint32_t msg_id;
-};
-
-struct MsgGameBeacon
-{
-	uint32_t msg_id;
-	char name[MP_SESSION_NAME_LEN];
-	char password;
-};
-
-struct MsgRequestGameList
-{
-	uint32_t msg_id;
-	uint32_t ack;
-};
-
-struct remote_game
-{
-	char name[MP_SESSION_NAME_LEN];
-	char password;
-	uint32_t host;
-	uint16_t port;
-};
-
-struct MsgGameList
-{
-	uint32_t msg_id;
-	uint32_t page;
-	uint32_t total_pages;
-	struct remote_game list[MP_GAME_LIST_SIZE];
-};
-
-struct MsgVersionAck
-{
-	uint32_t msg_id;
-};
-
-struct MsgVersionNak
-{
-	uint32_t msg_id;
-	uint32_t major;
-	uint32_t medium;
-	uint32_t minor;
-};
-
-struct MsgConnect
-{
-	uint32_t msg_id;
-	uint32_t player_id;
-	char name[MP_SESSION_NAME_LEN];
-	char password[MP_SESSION_NAME_LEN];
-};
-
-struct MsgConnectAck
-{
-	uint32_t msg_id;
-	uint32_t your_id;
-};
-
-struct MsgRequestLadder
-{
-	uint32_t msg_id;
-};
-
-struct ladder_entry
-{
-	char name[MP_PLAYER_NAME_LEN];
-	uint16_t wins;
-	uint16_t losses;
-	int32_t score;
-};
-
-struct MsgLadder
-{
-	uint32_t msg_id;
-	struct ladder_entry list[MP_LADDER_LIST_SIZE];
-};
-
-struct MsgNewPeerAddress
-{
-	uint32_t msg_id;
-	uint32_t player_id;
-	uint32_t host;
-	uint16_t port;
-};
-#pragma pack()
 
 enum ProtocolType
 {
@@ -169,14 +49,15 @@ enum ProtocolType
 
 struct SessionDesc
 {
-	char session_name[MP_SESSION_NAME_LEN+1];
-	char password[MP_SESSION_NAME_LEN+1];
+	char session_name[MP_FRIENDLY_NAME_LEN+1];
+	char password[MP_FRIENDLY_NAME_LEN+1];
 	uint32_t id;
-	struct inet_address address;
+	ENetAddress address;
 
 	SessionDesc();
 	SessionDesc(const SessionDesc &);
 	SessionDesc& operator= (const SessionDesc &);
+	SessionDesc(const char *name, const char *pass, ENetAddress *address);
 
 	char *name_str() { return session_name; };
 	uint32_t session_id() { return id; }
@@ -189,12 +70,10 @@ private:
 
 	int               init_flag;
 	int               lobbied_flag;
-	int               status;
 
 	ProtocolType      supported_protocols;
 	DynArrayB         current_sessions;
 	SessionDesc       joined_session;
-	Network           *network;
 
 	uint32_t          my_player_id;
 	PlayerDesc        *my_player;
@@ -206,13 +85,11 @@ private:
 	PlayerDesc        *player_pool[MAX_NATION];
 
 	char *            recv_buf;
-	std::queue<struct mp_msg *> recv_queue;
 
-	int               game_sock;
-	int               standard_port;
+	ENetHost          *host;
 
-	struct inet_address lan_broadcast_address;
-	struct inet_address remote_session_provider_address;
+	ENetAddress       lan_broadcast_address;
+	ENetAddress       remote_session_provider_address;
 
 	int use_remote_session_provider;
 	int update_available;
@@ -235,7 +112,6 @@ public:
 	void   poll_supported_protocols(); // can be called before init
 	bool   is_protocol_supported(ProtocolType);
 	int    is_update_available();
-	int    is_pregame();
 	void   game_starting();
 
 	// ------- functions on session --------//
@@ -243,52 +119,34 @@ public:
 	int    poll_sessions();
 	void   sort_sessions(int sortType);
 	int    create_session(char *sessionName, char *password, char *playerName, int maxPlayers);
-	int    join_session(int i, char *password, char *playerName);
+	int    join_session(SessionDesc *session, char *playerName);
 	void   close_session();
 	SessionDesc* get_session(int i);
+	SessionDesc *get_current_session();
 
 	// -------- functions on player management -------//
-	int         add_player(char *name, uint32_t id);
-	void        set_my_player_id(uint32_t id);
+	int         add_player(uint32_t id, char *name, ENetAddress *address, char contact);
+	int         auth_player(uint32_t id, char *name, char *password);
+	int         set_my_player_id(uint32_t id);
 	void        set_player_name(uint32_t id, char *name);
 	void        delete_player(uint32_t id);
 	void        poll_players();
 	PlayerDesc* get_player(int i);
+	PlayerDesc* get_player(ENetAddress *address);
 	PlayerDesc* search_player(uint32_t playerId);
 	int         is_player_connecting(uint32_t playerId);
 	int         get_player_count();
 	uint32_t    get_my_player_id() const { return my_player_id; }
-	void        set_peer_address(uint32_t who, struct inet_address *address);
 
 	// ------- functions on message passing ------//
 	int    send(uint32_t to, void * data, uint32_t msg_size);
-	int    send_stream(uint32_t to, void * data, uint32_t msg_size);
-	char * receive(uint32_t * from, uint32_t * to, uint32_t * size, int *sysMsgCount=0);
-	char * receive_stream(uint32_t * from, uint32_t * to, uint32_t * size, int *sysMsgCount=0);
-
-	int show_leader_board();
-	void yield();
+	char  *receive(uint32_t *from, uint32_t *size, int *sysMsgCount=0);
 
 private:
-	int open_port();
-	int open_standard_port(int fallback);
+	int open_port(uint16_t port, int fallback);
 
-	int send_nonseq_msg(int sock, char *msg, int msg_size, struct inet_address *to);
-	int send_system_msg(int sock, char *msg, int msg_size, struct inet_address *to);
-
-	int create_player(char *name, struct inet_address *address);
-	int get_player_id(struct inet_address *address);
-	int check_duplicates(struct inet_address *address);
-	void msg_game_beacon(MsgGameBeacon *m, struct inet_address *addr);
-	int msg_game_list(MsgGameList *m, int last_ack, struct inet_address *addr);
-	void msg_version_nak(MsgVersionNak *p, struct inet_address *addr);
-
-	void udp_accept_connections(struct packet_header *h, struct inet_address *address);
-	void send_game_beacon();
-
-	void yield_recv();
-	void yield_connecting();
-	void yield_pregame();
+	PlayerDesc *create_player(ENetAddress *address);
+	ENetPeer *get_peer(uint32_t id);
 };
 
 extern MultiPlayer mp_obj;

include/player_desc.h
@@ -26,7 +26,7 @@
 #define __PLAYER_DESC_H
 
 #include <MPTYPES.h>
-#include <network.h>
+#include <enet/enet.h>
 
 class PlayerDesc
 {
@@ -34,15 +34,17 @@ public:
 	uint32_t id;
 	char name[MP_FRIENDLY_NAME_LEN+1];
 	char connecting;
-	struct inet_address address;
+	char authorized;
+	ENetAddress address;
 
 	PlayerDesc();
-	PlayerDesc(const char *name);
-	PlayerDesc(uint32_t id, const char *name, struct inet_address *addr);
+	PlayerDesc(uint32_t id, ENetAddress *address);
+	PlayerDesc(uint32_t id, char *name);
+	PlayerDesc(char *name);
 
 	uint32_t pid();
 	char *friendly_name_str();
-	int get_address(struct inet_address *addr);
+	ENetAddress *get_address();
 };
 
 #endif // __PLAYER_DESC_H

src/client/Makefile.am
@@ -271,14 +271,6 @@ endif
 
 7kaa_LDADD += ../multiplayer/common/libmultiplayer.a
 
-if NET_NONE
-7kaa_LDADD += ../multiplayer/none/libnetwork_none.a
-endif
-
-if NET_SDLNET
-7kaa_LDADD += ../multiplayer/sdlnet/libnetwork_sdlnet.a
-endif
-
 
 if VIDEO_DDRAW
 7kaa_LDADD += ../video/ddraw/libvideo_ddraw.a

src/multiplayer/Makefile.am
@@ -1,6 +1,1 @@
 SUBDIRS = common
-if NET_SDLNET
-  SUBDIRS += sdlnet
-else
-  SUBDIRS += none
-endif

src/multiplayer/common/OERRCTRL.cpp
@@ -210,7 +210,7 @@ void ErrorControl::yield()
 	// -------- receive any frame from dplay -----------//
 	char *recvPtr;
 	uint32_t recvLen;
-	PID_TYPE from, to;
+	PID_TYPE from;
 
 	static int simError = 1;
 
@@ -229,7 +229,7 @@ void ErrorControl::yield()
 		}
 	}
 
-	while( (recvPtr = mp_ptr->receive(&from, &to, &recvLen, &sysMsgCount)) != NULL
+	while( (recvPtr = mp_ptr->receive(&from, &recvLen, &sysMsgCount)) != NULL
 		|| sysMsgCount != 0)
 	{
 		// -------- detect any player lost ---------//

commit 489388ed7c45571da92757af0a3d02be0442a619

Multiplayer: Fixed major sync issue in is_mp_sync
This issue was introduced in "96101eed6ad Implement network support using enet"
is_mp_sync was not waiting when not all players were ready.

src/client/OSYS.cpp
@@ -1125,7 +1125,7 @@ int Sys::is_mp_sync(int *unreadyPlayerFlag)
 		&& remote.has_send_frame(nation_array.player_recno, frame_count)
 		&& (~nation_array)->next_frame_ready==0 )
    {
-      DEBUG_LOG("Local player not ready");
+      //DEBUG_LOG("Local player not ready");
       if( !should_next_frame() )    // not ready to proceed yet
          return 0;
 
@@ -1160,7 +1160,7 @@ int Sys::is_mp_sync(int *unreadyPlayerFlag)
 
       if( remote_send_success_flag )      // still failed, try again next time
       {
-         DEBUG_LOG("first send sucess" );
+         DEBUG_LOG("First send success" );
          remote.init_send_queue(frame_count+1, nation_array.player_recno);    // frame_count, initialize for next frame's send queue
          // sent random seed
          char *p = (char *)remote.new_send_queue_msg(MSG_TELL_RANDOM_SEED, sizeof(short)+sizeof(long));
@@ -1244,15 +1244,18 @@ int Sys::is_mp_sync(int *unreadyPlayerFlag)
 
    //------- if some remote machines are not ready yet -------//
 
-   if( nationRecno>0 && !ec_remote.is_player_valid(nationRecno) )
+   if( nationRecno>0 )
    {
-      DEBUG_LOG("Connection Lost");
-      DEBUG_LOG(nationRecno);
+      if ( !ec_remote.is_player_valid(nationRecno) )
+      {
+         DEBUG_LOG("Connection Lost");
+         DEBUG_LOG(nationRecno);
 
-      //---- the connection was lost with a remote player, let the ai take over ----//
-      news_array.multi_connection_lost(nationRecno);
-      nationPtr->nation_type = NATION_AI;
-      nation_array.ai_nation_count++;
+         //---- the connection was lost with a remote player, let the ai take over ----//
+         news_array.multi_connection_lost(nationRecno);
+         nationPtr->nation_type = NATION_AI;
+         nation_array.ai_nation_count++;
+      }
 
       return 0;
    }