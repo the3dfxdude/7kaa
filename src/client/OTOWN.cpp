the3dfxdude/7kaa

commit c390594d246acedbbfee0b3fd5427acb8498e1d4

Town: Fixed a typo causing quality of life not being added to the target loyalty as intended

src/client/OTOWN.cpp
@@ -1199,7 +1199,7 @@ void Town::update_target_loyalty()
 		//------- calculate the target loyalty -------//
 
 		targetLoyalty = race_harmony(i+1)/3 +				// 0 to 33
-							 (int)nationPtr->reputation/4;	// -25 to +25
+							 (int)nationPtr->reputation/4 +	// -25 to +25
 							 (quality_of_life-50)/3;				// -17 to +17
 
 		//---- employment help increase loyalty ----//

commit 2d2cd5ab013304030130408da7302e1a7797ceb4

Do not apply Quality-of-Life below 30 loyalty.
This change fixes a balancing issue introduced in c390594d246,
which introduced the quality of life, a quantity which influences the loyalty of towns.
Fixes: towns without a fort (such as towns that surrender to you from another player)
no longer rebel after only a short time due to target loyalty becoming less than 30.

src/client/OTOWN.cpp
@@ -1217,8 +1217,7 @@ void Town::update_target_loyalty()
 		//------- calculate the target loyalty -------//
 
 		targetLoyalty = race_harmony(i+1)/3 +				// 0 to 33
-							 (int)nationPtr->reputation/4 +	// -25 to +25
-							 (quality_of_life-50)/3;				// -17 to +17
+							 (int)nationPtr->reputation/4;	// -25 to +25
 
 		//---- employment help increase loyalty ----//
 
@@ -1302,6 +1301,24 @@ void Town::update_target_loyalty()
 				targetLoyalty = race_target_loyalty_array[j] - thisInfluence;
 				race_target_loyalty_array[j] = MAX(0, targetLoyalty);
 			}
+		}
+	}
+
+	//------- apply quality of life -------//
+
+	int qolContribution = (quality_of_life-50)/3;			// -17 to +17
+	for( i=0 ; i<MAX_RACE ; i++ )
+	{
+		if( race_pop_array[i] == 0 )
+			continue;
+
+		targetLoyalty = race_target_loyalty_array[i];
+
+		// Quality of life only applies to the part above 30 loyalty
+		if (targetLoyalty > 30)
+		{
+			targetLoyalty = MAX(30, targetLoyalty + qolContribution);
+			race_target_loyalty_array[i] = MIN(100, targetLoyalty);
 		}
 	}
 