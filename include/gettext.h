9cc0b5e0e0dd28a98a960bdfda00f928dca482fa
Jesse Allen <the3dfxdude@gmail.com>
2015-07-04 18:40:34 -0700

    gettext: Fix !ENABLE_NLS on MSVC and Mingw32.

--- include/gettext.h
+++ include/gettext.h
@@ -24,6 +24,49 @@
 #ifndef __GETTEXT_H_WRAPPER
 #define __GETTEXT_H_WRAPPER
 
+// gettext-gnu.h won't define LC_MESSAGES when !defined ENABLE_NLS (happens
+// when skipping libintl.h) but tries to use LC_MESSAGES anyway.
+// gettext-gnu.h assumes we have a good locale.h, but that is not always the
+// case.
+#if (! defined ENABLE_NLS && ! defined LC_MESSAGES)
+#define LC_MESSAGES 1729
+#endif
+
+#if defined(_MSC_VER) && _MSC_VER < 1900
+
+#include <stdio.h>
+
+// C99 snprintf compatibility for MSVC earlier than 2015
+// credit: http://stackoverflow.com/questions/2915672/snprintf-and-visual-studio-2010
+#define snprintf c99_snprintf
+#define vsnprintf c99_vsnprintf
+
+inline int c99_vsnprintf(char *outBuf, size_t size, const char *format, va_list ap)
+{
+	int count = -1;
+
+	if (size != 0)
+		count = _vsnprintf_s(outBuf, size, _TRUNCATE, format, ap);
+	if (count == -1)
+		count = _vscprintf(format, ap);
+
+	return count;
+}
+
+inline int c99_snprintf(char *outBuf, size_t size, const char *format, ...)
+{
+	int count;
+	va_list ap;
+
+	va_start(ap, format);
+	count = c99_vsnprintf(outBuf, size, format, ap);
+	va_end(ap);
+
+	return count;
+}
+
+#endif // _MSC_VER < 1900
+
 #ifdef USE_NEW_GETTEXT_H
 
 #include "gettext-gnu.h"
