045b09d65f15fcebb55263925349f751f044850f
sraboy <steven.lavoiejr@gmail.com>
2016-02-25 16:43:46 -1000

    Fix button alignment

--- src/client/OF_MINE.cpp
+++ src/client/OF_MINE.cpp
@@ -198,7 +198,7 @@ void FirmMine::put_info(int refreshFlag)
 
 	if (own_firm() && refreshFlag == INFO_REPAINT)
 	{
-		button_vacate_firm.paint(INFO_X1 + BUTTON_ACTION_WIDTH, INFO_Y1 + 248, 'A', "RECRUIT");
+		button_vacate_firm.paint(INFO_X1, INFO_Y1 + 249, 'A', "RECRUIT");
 		button_vacate_firm.set_help_code("MOBILIZE");
 
 		if (worker_count)
