4cf787fb12edc86579029e0f6365e8dca12bcfa7
Richard Dijk <microvirus.multiplying@gmail.com>
2016-05-25 14:51:51 +0200

    OERROR: Also set debugger breakpoint in Error::internal and Error::mem for Microsoft Visual C++.
    This extends commit 206d69b "OERROR: Set debugger breakpoint in Error::run for Microsoft Visual C++."

--- src/OERROR.cpp
+++ src/OERROR.cpp
@@ -96,6 +96,11 @@ static void new_func_handler()
 //
 void Error::internal(char* errMsg,const char* fileName,int lineNum)
 {
+#if defined(_MSC_VER) && defined(_DEBUG)
+	// Let the debugger have a first shot at the error when in debug build, before clean-up happens.
+	__debugbreak();
+#endif
+
  	if( error_flag )	// prevent error message dead loop
 		return;
 
@@ -137,6 +142,11 @@ void Error::internal(char* errMsg,const char* fileName,int lineNum)
 //
 void Error::mem()
 {
+#if defined(_MSC_VER) && defined(_DEBUG)
+	// Let the debugger have a first shot at the error when in debug build, before clean-up happens.
+	__debugbreak();
+#endif
+
 	if( error_flag )	// prevent error message dead loop
 		return;
 
