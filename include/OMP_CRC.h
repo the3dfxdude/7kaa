c15e39652be5e7f0a6d3879c549b96f4843abb2f
Jesse Allen <the3dfxdude@gmail.com>
2018-10-28 19:56:46 -0700

    Add header.

--- include/OMP_CRC.h
+++ include/OMP_CRC.h
@@ -1,3 +1,26 @@
+/*
+ * Seven Kingdoms: Ancient Adversaries
+ *
+ * Copyright 2018 Jesse Allen
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+//Filename    : OMP_CRC.H
+//Description : crc checking for multiplayer debugging
+
 #ifndef _OMP_CRC_H
 #define _OMP_CRC_H
 
