the3dfxdude/7kaa

commit 3470b748470243aa62f567fcfa56657991ea75fe

SaveGameArray: Separate the information about the file from the header of the file.
Couple points:
* Raw time information is not portable across systems. No reason to store a raw time in the SaveGame for this reason too.
* The game never used the file information stored in the save game anyway.

Added support for POSIX for the file time.

src/OSaveGameArray.cpp
@@ -100,7 +100,7 @@ static void    disp_scroll_bar_func(SlideVBar *scroll, int);
 
 //------ Begin of function SaveGameArray constuctor ------//
 
-SaveGameArray::SaveGameArray() : DynArray( sizeof(SaveGameInfo), 10 )
+SaveGameArray::SaveGameArray() : DynArray( sizeof(SaveGame), 10 )
 {
 	has_fetched_last_file_name_from_hall_of_fame = false;
 	last_file_name[0] = '\0';
@@ -251,7 +251,7 @@ int SaveGameArray::menu(int actionMode, int *recno)
 
 	for( int i=1 ; i<=size() ; i++ )
 	{
-		if( strcmp(last_file_name, (*this)[i]->file_name)==0 )
+		if( strcmp(last_file_name, (*this)[i]->file_info.name)==0 )
 		{
 			browse_recno = i;
 			break;
@@ -592,15 +592,15 @@ void SaveGameArray::disp_browse()
 
 //-------- Begin of function SaveGameArray::disp_entry_info --------//
 //
-void SaveGameArray::disp_entry_info(const SaveGameInfo* entry, int x, int y)
+void SaveGameArray::disp_entry_info(const SaveGame* entry, int x, int y)
 {
-	vga_front.put_bitmap(x+10, y+10,	unit_res[ race_res[entry->race_id]->basic_unit_id ]->king_icon_ptr);
+	vga_front.put_bitmap(x+10, y+10,	unit_res[ race_res[entry->header.race_id]->basic_unit_id ]->king_icon_ptr);
 
 	x+=60;
 
 	//------ display player color -----//
 
-	nation_array.disp_nation_color( x+1, y+13, entry->nation_color );
+	nation_array.disp_nation_color( x+1, y+13, entry->header.nation_color );
 
 	//-------- display king name ------//
 
@@ -608,21 +608,21 @@ void SaveGameArray::disp_entry_info(const SaveGameInfo* entry, int x, int y)
 
 	str  = _("King");
 	str += " ";
-	str += entry->player_name;
+	str += entry->header.player_name;
 
 	font_bible.put( x+18, y+8, str );
 
 	//------- display game date --------//
 
 	str  = _("Game Date: ");
-	str += date.date_str(entry->game_date);
+	str += date.date_str(entry->header.game_date);
 
 	font_bible.put( x, y+30, str );
 
 	//---------------------------------//
 
 	str  = _("File Name: ");
-	str += entry->file_name;
+	str += entry->file_info.name;
 
 	#if(defined(FRENCH))
 		font_small.put( x+320, y+16, str );
@@ -632,25 +632,21 @@ void SaveGameArray::disp_entry_info(const SaveGameInfo* entry, int x, int y)
 		font_small.put( x+335, y+16, str );
 	#endif
 
-	int timeYear, timeMonth, timeDay, timeHour, timeMinute;
+	str  = _("File Date: ");
 #ifdef USE_WINDOWS
-	FILETIME fileTime;
 	FILETIME localFileTime;
 	SYSTEMTIME sysTime;
-	fileTime.dwLowDateTime = static_cast<std::uint32_t>(entry->file_date);
-	fileTime.dwHighDateTime = static_cast<std::uint32_t>(entry->file_date >> 32);
-	FileTimeToLocalFileTime( &fileTime, &localFileTime );
+	FileTimeToLocalFileTime( &entry->file_info.time, &localFileTime );
 	FileTimeToSystemTime( &localFileTime, &sysTime );
-	timeYear = sysTime.wYear; timeMonth = sysTime.wMonth; timeDay = sysTime.wDay;
-	timeHour = sysTime.wHour; timeMinute = sysTime.wMinute;
-#else // FIXME
-	timeYear = timeMonth = timeDay = timeHour = timeMinute = 0;
-#endif
-
-	str  = _("File Date: ");
-	str += date.date_str(date.julian(timeYear, timeMonth, timeDay), 1);
+	str += date.date_str(date.julian(sysTime.wYear, sysTime.wMonth, sysTime.
+wDay), 1);
 	str += " ";
-	str += date.time_str( timeHour * 100 + timeMinute );
+	str += date.time_str(sysTime.wHour * 100 + sysTime.wMinute);
+#elif defined USE_POSIX
+	str += date.date_str(date.julian(entry->file_info.time.tm_year+1900, entry->file_info.time.tm_mon+1, entry->file_info.time.tm_mday), 1);
+	str += " ";
+	str += date.time_str(entry->file_info.time.tm_hour*100 + entry->file_info.time.tm_min);
+#endif
 
 	#if(defined(FRENCH))
 		font_small.put( x+318, y+34, str );
@@ -688,14 +684,14 @@ int SaveGameArray::process_action(int saveNew)
 			if( !box.ask( _("It will overwrite the existing saved game. Proceed?") ) )
 				return 0;
 
-			SaveGameInfo* saveGameInfo = (*this)[browse_recno];
-			if( !SaveGameProvider::save_game(saveGameInfo->file_name, /*out*/ saveGameInfo) )
+			SaveGame* saveGame = (*this)[browse_recno];
+			if( !SaveGameProvider::save_game(saveGame->file_info.name, /*out*/ &saveGame->header) )
 			{
 				box.msg(GameFile::status_str());
 				return -1;
 			}
 
-			strcpy( last_file_name, saveGameInfo->file_name );
+			strcpy( last_file_name, saveGame->file_info.name );
 		}
 
 		return 1;
@@ -705,12 +701,12 @@ int SaveGameArray::process_action(int saveNew)
 
 	else
 	{
-		SaveGameInfo* saveGameInfo = (*this)[browse_recno];
+		SaveGame* saveGame = (*this)[browse_recno];
 
-		int rc = SaveGameProvider::load_game(saveGameInfo->file_name, /*out*/ saveGameInfo);
+		int rc = SaveGameProvider::load_game(saveGame->file_info.name, /*out*/ &saveGame->header);
 		if( rc > 0 )
 		{
-			strcpy( last_file_name, saveGameInfo->file_name );
+			strcpy( last_file_name, saveGame->file_info.name);
 		}
 		else
 		{
@@ -748,9 +744,9 @@ int SaveGameArray::save_new_game(const char* newFileName)
 
 		for( gameFileRecno=1 ; gameFileRecno<=this->size() ; gameFileRecno++ )
 		{
-			SaveGameInfo* saveGameInfoPtr = (*this)[gameFileRecno];
+			SaveGame* saveGame = (*this)[gameFileRecno];
 
-			if( strcmp(saveGameInfoPtr->file_name, newFileName)==0 )      // if this file name already exist
+			if( strcmp(saveGame->file_info.name, newFileName)==0 )      // if this file name already exist
 			{
 				addFlag=0;
 				break;
@@ -766,20 +762,20 @@ int SaveGameArray::save_new_game(const char* newFileName)
 
 	//----------- save game now ------------//
 
-	SaveGameInfo saveGameInfo;
-	if( SaveGameProvider::save_game(fileName, /*out*/ &saveGameInfo) )
+	SaveGame saveGame;
+	if( SaveGameProvider::save_game(fileName, /*out*/ &saveGame.header) )
 	{
-		strcpy( last_file_name, saveGameInfo.file_name );
+		strcpy( last_file_name, saveGame.file_info.name );
 
 		if( addFlag )
 		{
-			linkin(&saveGameInfo);
+			linkin(&saveGame);
 
 			quick_sort( sort_game_file_function );
 		}
 		else
 		{
-			this->update(&saveGameInfo, gameFileRecno);
+			this->update(&saveGame, gameFileRecno);
 		}
 
 		return 1;
@@ -839,10 +835,10 @@ void SaveGameArray::set_file_name(char* /*out*/ fileName, int size)
 
 	for( i=1 ; i<=this->size() ; i++ )
 	{
-		SaveGameInfo* findSaveGameInfo = (*this)[i];
+		SaveGame* findSaveGame = (*this)[i];
 
 		// ##### begin Gilbert 3/10 ########//
-		if( strnicmp(findSaveGameInfo->file_name, str, NAME_PREFIX_LEN)==0 )
+		if( strnicmp(findSaveGame->file_info.name, str, NAME_PREFIX_LEN)==0 )
 			// ##### end Gilbert 3/10 ########//
 		{
 			//------------------------------------------------//
@@ -852,7 +848,7 @@ void SaveGameArray::set_file_name(char* /*out*/ fileName, int size)
 			//
 			//------------------------------------------------//
 
-			curNumber = atoi( findSaveGameInfo->file_name+NAME_PREFIX_LEN+1 );      // +1 is to pass the "_" between the name and the number
+			curNumber = atoi( findSaveGame->file_info.name+NAME_PREFIX_LEN+1 );      // +1 is to pass the "_" between the name and the number
 
 			if( curNumber > lastNumber+1 )   // normally, curNumber should be lastNumber+1
 				break;
@@ -888,7 +884,7 @@ void SaveGameArray::del_game()
 	if( !box.ask( _("This saved game will be deleted. Proceed?") ) )
 		return;
 
-	SaveGameProvider::delete_savegame((*this)[recNo]->file_name);
+	SaveGameProvider::delete_savegame((*this)[recNo]->file_info.name);
 
 	go(recNo);
 	linkout();
@@ -906,8 +902,8 @@ void SaveGameArray::load_all_game_header(const char *extStr)
 {
 	zap();
 
-	SaveGameProvider::enumerate_savegames(extStr, [this](const SaveGameInfo* saveGameInfo) {
-		this->linkin(saveGameInfo);
+	SaveGameProvider::enumerate_savegames(extStr, [this](const SaveGame* saveGame) {
+		this->linkin(saveGame);
 	});
 
 	quick_sort( sort_game_file_function );
@@ -923,29 +919,29 @@ static int sort_game_file_function( const void *a, const void *b )
 {
 	int firstAuto = 0, secondAuto = 0;
 
-	firstAuto = strcmpi( ((SaveGameInfo*)a)->file_name, "AUTO.SAV" ) == 0 ? 1 :
-		(strcmpi( ((SaveGameInfo*)a)->file_name, "AUTO2.SAV" ) == 0 ? 2 : 0);
+	firstAuto = strcmpi( ((SaveGame*)a)->file_info.name, "AUTO.SAV" ) == 0 ? 1 :
+		(strcmpi( ((SaveGame*)a)->file_info.name, "AUTO2.SAV" ) == 0 ? 2 : 0);
 	if (firstAuto != 1) // only check second if first is not AUTO.SAV
-		secondAuto = strcmpi( ((SaveGameInfo*)b)->file_name, "AUTO.SAV" ) == 0 ? 1 :
-			(strcmpi( ((SaveGameInfo*)b)->file_name, "AUTO2.SAV" ) == 0 ? 2 : 0);
+		secondAuto = strcmpi( ((SaveGame*)b)->file_info.name, "AUTO.SAV" ) == 0 ? 1 :
+			(strcmpi( ((SaveGame*)b)->file_info.name, "AUTO2.SAV" ) == 0 ? 2 : 0);
 
 	if (firstAuto == 1 || (firstAuto == 2 && secondAuto != 1))
 		return -1;
 	else if (secondAuto > 0)
 		return 1;
 	else
-		return strcmpi( ((SaveGameInfo*)a)->file_name, ((SaveGameInfo*)b)->file_name );
+		return strcmpi( ((SaveGame*)a)->file_info.name, ((SaveGame*)b)->file_info.name );
 }
 //------- End of function sort_game_file_function ------//
 
 
 //------- Begin of function SaveGameArray::operator[] -----//
 
-SaveGameInfo* SaveGameArray::operator[](int recNo)
+SaveGame* SaveGameArray::operator[](int recNo)
 {
-	SaveGameInfo* saveGameInfo = (SaveGameInfo*) get(recNo);
+	SaveGame* saveGame = (SaveGame*) get(recNo);
 
-	return saveGameInfo;
+	return saveGame;
 }
 //--------- End of function SaveGameArray::operator[] ----//
 

include/ODIR.h
@@ -27,7 +27,12 @@
 #include <storage_constants.h>
 #include <ODYNARR.h>
 
-#include <cstdint>
+#ifdef USE_WINDOWS
+#include <windows.h>
+#endif
+#ifdef USE_POSIX
+#include <time.h>
+#endif
 
 //---------- Define struct FileInfo ----------//
 
@@ -35,7 +40,12 @@ struct FileInfo
 {
     char          name[MAX_PATH+1];
     unsigned long size;
-    std::uint64_t time;
+#ifdef USE_WINDOWS
+    FILETIME      time;
+#endif
+#ifdef USE_POSIX
+    struct tm     time;
+#endif
 };
 
 //---------- Define class Directory ----------//

include/OSaveGameArray.h
@@ -28,8 +28,14 @@
 #include <ODYNARR.h>
 #endif
 
+#include <ODIR.h>
 #include <OSaveGameInfo.h>
 
+struct SaveGame
+{
+	FileInfo file_info;
+	SaveGameInfo header;
+};
 
 class SaveGameArray : private DynArray
 {
@@ -46,13 +52,13 @@ public:
 
 	int save_new_game(const char* =NULL); // save a new game immediately without prompting menu
 
-	SaveGameInfo* operator[](int recNo);
+	SaveGame* operator[](int recNo);
 
 private:
 	void set_file_name(char* /*out*/ fileName, int size);
 
 	void disp_browse();
-	static void disp_entry_info(const SaveGameInfo* entry, int x, int y);
+	static void disp_entry_info(const SaveGame* entry, int x, int y);
 	void load_all_game_header(const char *extStr);
 	int  process_action(int=0);
 	void del_game();

include/OSaveGameInfo.h
@@ -28,23 +28,30 @@
 
 #include <cstdint>
 
+#ifdef USE_WINDOWS
+#include <windows.h>
+#else
+typedef struct _FILETIME {
+	uint32_t dwLowDateTime;
+	uint32_t dwHighDateTime;
+} FILETIME, *PFILETIME;
+#endif
 
 // The basic information ('header') of a savegame.
 #pragma pack(1)
 struct SaveGameInfo
 {
-	char     file_name[MAX_PATH+1];
+	char     file_name[MAX_PATH+1]; // unused
 	char     player_name[HUMAN_NAME_LEN+1];
 
 	char     race_id;
 	char     nation_color;
 
 	int           game_date;      // the game date of the saved game
-	std::uint64_t file_date;      // saving game date (FILETIME)
+	FILETIME      file_date;      // unused
 	short         terrain_set;
 };
 #pragma pack()
-
 
 SaveGameInfo SaveGameInfoFromCurrentGame(const char* newFileName);
 

include/OSaveGameProvider.h
@@ -28,6 +28,7 @@
 #include <functional>
 
 class String;
+struct SaveGame;
 struct SaveGameInfo;
 
 // Note on nomenclature:
@@ -43,7 +44,7 @@ class SaveGameProvider
 {
 public:
 	//  Enumerates all the savegames, calling callback for each savegame.
-	static void enumerate_savegames(const char* filenameWildcard, const std::function<void (const SaveGameInfo* saveGameInfo)>& callback);
+	static void enumerate_savegames(const char* filenameWildcard, const std::function<void (const SaveGame* saveGame)>& callback);
 
 	// Deletes the savegame whose file part of filename is saveGameName.
 	static void delete_savegame(const char* saveGameName);

src/ODIR.cpp
@@ -99,20 +99,24 @@ int Directory::read(const char *fileSpec, int sortName)
       struct stat file_stat;
       char *p;
 
-      stat(results.gl_pathv[i], &file_stat);
+      if( stat(results.gl_pathv[i], &file_stat) )
+      {
+         // can't read, skip
+         continue;
+      }
 
       p = strrchr(results.gl_pathv[i], PATH_DELIM[0]);
       if( p )
          p++;
       else
          p = results.gl_pathv[i];
-
       size_t filename_len = strlen(p);
       if( filename_len >= MAX_PATH )
          continue;
+
       memcpy(fileInfo.name, p, filename_len+1);
       fileInfo.size = file_stat.st_size;
-      fileInfo.time = 0; // FIXME
+      fileInfo.time = *localtime(&file_stat.st_mtime);
 
       linkin(&fileInfo);
    }

src/OGAMMAIN.cpp
@@ -864,7 +864,7 @@ void Game::multi_player_menu(int lobbied, char *game_host)
 							{
 								err_when( !loadedRecno );
 								// ####### begin Gilbert 13/2 #######//
-								load_mp_game(save_game_array[loadedRecno]->file_name, lobbied, game_host);
+								load_mp_game(save_game_array[loadedRecno]->file_info.name, lobbied, game_host);
 								// ####### begin Gilbert 13/2 #######//
 							}
 							{

src/OSaveGameInfo.cpp
@@ -26,17 +26,12 @@
 #include <OCONFIG.h>
 #include <OINFO.h>
 
-#ifdef USE_WINDOWS
-#include <windows.h>
-#endif
-
 
 SaveGameInfo SaveGameInfoFromCurrentGame(const char* newFileName)
 {
 	SaveGameInfo saveGameInfo;
 
-	strncpy( saveGameInfo.file_name, newFileName, MAX_PATH);
-	saveGameInfo.file_name[MAX_PATH] = '\0';
+	memset(saveGameInfo.file_name, 0, MAX_PATH+1);
 
 	Nation* playerNation = ~nation_array;
 	strncpy( saveGameInfo.player_name, playerNation->king_name(), HUMAN_NAME_LEN );
@@ -46,17 +41,11 @@ SaveGameInfo SaveGameInfoFromCurrentGame(const char* newFileName)
 	saveGameInfo.nation_color = playerNation->nation_color;
 
 	saveGameInfo.game_date    = info.game_date;
-	saveGameInfo.file_date    = 0;
-#ifdef USE_WINDOWS
+
 	//----- set the file date ------//
 
-	FILETIME sysTimeAsFileTime;
-	SYSTEMTIME sysTime;
-	GetSystemTime(&sysTime);
-	SystemTimeToFileTime(&sysTime, &sysTimeAsFileTime);
-	saveGameInfo.file_date = static_cast<std::uint64_t>(sysTimeAsFileTime.dwHighDateTime) << 32 | sysTimeAsFileTime.dwLowDateTime;
-#else // FIXME
-#endif
+	saveGameInfo.file_date.dwLowDateTime = 0;
+	saveGameInfo.file_date.dwHighDateTime = 0;
 
 	saveGameInfo.terrain_set  = config.terrain_set;
 

commit ac84af6686ab759a5675e4f6584b3174e713525c

Fixed crash (buffer overflow) with Save New Game introduced in 3470b748 "SaveGameArray: Separate the information about the file from the header of the file."
The file_info part of the saved game was never initialised, in particular the file_name part wasn't, which causes random memory to be overwritten in the strcpy.
There's still an open TODO of updating the file_info for both save and save new.

src/OSaveGameArray.cpp
@@ -760,7 +760,10 @@ int SaveGameArray::save_new_game(const char* newFileName)
 	SaveGame saveGame;
 	if( SaveGameProvider::save_game(fileName, /*out*/ &saveGame.header) )
 	{
-		strcpy( last_file_name, saveGame.file_info.name );
+		strcpy( saveGame.file_info.name, fileName );
+		saveGame.file_info.size = 0;
+		saveGame.file_info.time = TimeInfo{}; // TODO: We should create a correct file_info, here and also for the normal save.
+		strcpy( last_file_name, fileName );
 
 		if( addFlag )
 		{