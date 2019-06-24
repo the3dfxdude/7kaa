8d8d92e44986fa6e610b428e90229f0be15257d3
Jesse Allen <the3dfxdude@gmail.com>
2019-06-23 18:29:48 -0700

    Bump version

--- include/version.h
+++ include/version.h
@@ -27,11 +27,11 @@
 #ifndef __VERSION_H
 #define __VERSION_H
 
-#define SKVERSION "2.15.2"
+#define SKVERSION "2.15.3"
 #define SKVERMAJ 2
 #define SKVERMED 15
-#define SKVERMIN 2
+#define SKVERMIN 3
 
-//#define DEV_VERSION
+#define DEV_VERSION
 
 #endif
