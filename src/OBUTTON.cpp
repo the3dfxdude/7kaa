4b7c7da0a002fec74527f67c0d04ae758a4fccc9
Jesse Allen <the3dfxdude@gmail.com>
2019-08-28 12:06:18 -0700

    ButtonGroup: Run detect while waiting on a pushed button.

--- src/OBUTTON.cpp
+++ src/OBUTTON.cpp
@@ -570,7 +570,7 @@ int ButtonGroup::detect()
 
    for( i=0 ; i<button_num ; i++ )
    {
-		if( !button_array[i].is_pushed && button_array[i].detect() )
+      if( (!button_array[i].is_pushed || button_array[i].button_wait) && button_array[i].detect() )
       {
           button_array[button_pressed].pop();
           button_pressed = i;
