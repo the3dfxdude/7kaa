f4419336145b21939ffa7a62d3717c32ec853e7d
W-Damian <80742709+W-Damian@users.noreply.github.com>
2021-03-17 01:31:51 -0500

    Remove wiki reference from English text
    Ignore this pull request if the wiki will be reinstated.
    TODO: sync .pot file and edit translation.

--- src/OGAMEMP.cpp
+++ src/OGAMEMP.cpp
@@ -1738,7 +1738,7 @@ int Game::input_name_pass(const char *txt[], char *name, int name_len, char *pas
 #ifdef HAVE_LIBCURL
 const char *login_failed_msg = N_("Unable to connect to the 7kfans.com service. Verify your account information and try again.");
 #else
-const char *login_failed_msg = N_("Unable to connect to the 7kfans service. See 7kfans.com/wiki on how to log in.\n(No libcurl)");
+const char *login_failed_msg = N_("Unable to connect to the 7kfans service.\n(No libcurl)");
 #endif
 
 
